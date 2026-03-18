2a511e6aac86adbb1b6abd8f9070332edec2f48c
jiefong <jf.gan@morninggeek.com>
2026-03-18 19:33:57 +0800

    fix: apply passive gain for ascension upgrade 2

--- js/calc.js
+++ js/calc.js
@@ -15,5 +15,15 @@ function calc(dt) {
 
     player.mastery_essence = player.mastery_essence.add(tmp.essGain.mul(dt))
 
-    //roll()
+    // Passive Luck Essence generation (unlocks after Mastery Tier 5)
+    if (player.mastery_tier >= 5 && tmp.leGain) {
+        player.luck_essence = player.luck_essence.add(tmp.leGain.mul(dt))
+    }
+
+    if (hasUpgrade('as',2)) {
+        let asEff = upgradeEffect('as',2)
+        if (player.pTimes > 0) player.pp = player.pp.add(tmp.ppGain.mul(asEff).mul(dt))
+        if (player.tTimes > 0) player.tp = player.tp.add(tmp.tpGain.mul(asEff).mul(dt))
+        if (player.rTimes > 0) player.rp = player.rp.add(tmp.rpGain.mul(asEff).mul(dt))
+    }
 }
